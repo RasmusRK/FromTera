b33d19c0770ae2cab7e7d39b4c7db90126ddfb1c
Burger <wrz641@ku.dk>
2017-02-24 16:24:39 +0100

    added project

---
+++ From Tera/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject[] players;
+	private Vector3 offset;
+	private Vector3 newpos;
+
+	void Start ()
+	{
+		foreach(GameObject player in players)
+		{
+			offset += player.transform.position;
+		}
+		offset = transform.position - offset/players.Length;
+	}
+
+	void Awake()
+	{
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+
+	void LateUpdate ()
+	{
+		newpos = new Vector3(0,0,0);
+		foreach(GameObject player in players)
+		{
+			newpos += player.transform.position;
+		}
+		transform.position = newpos/players.Length + offset;
+	}
+}
