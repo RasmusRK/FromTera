a1198e91a125e450e58115c942bc88d5dc5e6c96
Kasper Passov <kasper.passov@gmail.com>
2017-02-25 15:18:28 +0100

    Added automatic zoom to multiplayer camera

--- FromTera/Assets/Scripts/CameraController.cs
+++ FromTera/Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour {
 	public GameObject[] players;
 	private Vector3 offset;
 	private Vector3 newpos;
+	private float furthest;
+	private Camera main_cam;
 
 	void Start ()
 	{
@@ -15,6 +17,8 @@ public class CameraController : MonoBehaviour {
 			offset += player.transform.position;
 		}
 		offset = transform.position - offset/players.Length;
+		furthest = 0;
+		main_cam = GetComponent<Camera> ();
 	}
 
 	void Awake()
@@ -24,11 +28,34 @@ public class CameraController : MonoBehaviour {
 
 	void LateUpdate ()
 	{
-		newpos = new Vector3(0,0,0);
-		foreach(GameObject player in players)
-		{
-			newpos += player.transform.position;
+		if (players.Length > 1) {
+			MultiplayerCamera ();
+		} else {
+			SinglePlayerCamera ();
 		}
+	}
+
+	private void SinglePlayerCamera()
+	{
+		transform.position = players[0].transform.position + offset;
+	}
+
+	private void MultiplayerCamera()
+	{
+		newpos = new Vector3(0,0,0);
+		furthest = 0;
+		foreach(GameObject player1 in players)
+		{
+			newpos += player1.transform.position;
+			foreach (GameObject player2 in players) {
+				if (player2 != player1 && furthest < (player1.transform.position - player2.transform.position).magnitude) {
+					furthest = (player1.transform.position - player2.transform.position).magnitude;
+
+				}
+			}
+		}
+		print (furthest);
+		main_cam.orthographicSize = Mathf.Max(furthest * (float) 0.6,7);
 		transform.position = newpos/players.Length + offset;
 	}
 }
