351e85e4745a3d4686561a9db38ea22a6205e4cc
Kasper Passov <kasper.passov@gmail.com>
2017-02-28 01:37:08 +0100

    Turrets!

---
+++ FromTera/Assets/Scripts/AI/ShootAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAtPlayer : MonoBehaviour {
+
+	public GameObject shotType;
+	public float shotDelay = 0.01f;
+
+	private GameObject player;
+	private float nextShotTime = 0f;
+
+	void Start () {
+		player =  GameObject.FindGameObjectWithTag("Player");	
+	}
+	
+	void Update () {		
+		if (Time.time > nextShotTime ) {
+			nextShotTime += shotDelay;
+			GameObject shot = Instantiate (shotType, transform.position, transform.rotation);
+			shot.GetComponent<Rigidbody2D> ().AddForce (transform.forward * 50);
+		}
+	}
+}
---
+++ FromTera/Assets/Scripts/AI/TurnToPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToPlayer : MonoBehaviour {
+
+	private GameObject player;
+	void Start () {
+		player =  GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () {
+		Vector3 diff = this.transform.position - player.transform.position;
+		diff.Normalize ();
+
+		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+	}
+}
